a882d0e2a6997f04ea18941b5e302b15f4ca2437
Claudia Provoste Pérez <cprovoste16@alumnos.utalca.cl>
2020-12-21 17:19:06 -0300

    New Script "BallPhysics"
    Gets the controller's ccenter of mass

---
+++ CarnivalVR/Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour
+{
+    Vector3 controllerCenterOfMass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerCenterOfMass = GetComponent<Rigidbody>().centerOfMass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
