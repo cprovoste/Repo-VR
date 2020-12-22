716c51e97abca9851a72040b7e89bc57eb1cc72c
Claudia Provoste Pérez <cprovoste16@alumnos.utalca.cl>
2020-12-22 19:31:20 -0300

    Controller Input
    First try at controllers inputs

---
+++ CarnivalVR/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handModelPrefab;
+    public List<GameObject> controllerPrefabs;
+    public bool showController = false;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach(var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if(prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Didn't find controller model.");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedController.SetActive(false);
+            spawnedHandModel.SetActive(true);
+        }
+    }
+}
